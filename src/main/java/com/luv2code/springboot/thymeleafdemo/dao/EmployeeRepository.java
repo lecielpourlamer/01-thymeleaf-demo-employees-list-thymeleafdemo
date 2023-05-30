7694a3a639ee8606c38b579572869757209e554c
ocean <shlim@repia.com>
2023-05-30 18:54:26 +0900

    Section7:NEW-Spring Boot 3 - Spring MVC CRUD finished

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+    // Spring Data JPA will parse the method name
+    // Looks for a specific format and pattern
+    // Creates appropriate query ...behind the scenes
+
 	
 }
